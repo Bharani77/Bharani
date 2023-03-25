0e032cf1049e0fcb8a9154f0665a5c0c22da5853
Bharani77 <bharani007dharan@gmail.com>
2023-03-25 12:59:02 +0530

    Update PPM report.java

--- PPM report.java
+++ PPM report.java
@@ -1,58 +1,85 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class CheckFileInNetworkPath {
+public class NetworkFileChecker {
 
     public static void main(String[] args) {
-        
-        // Define network shared path
-        String sharedPath = "\\\\servername\\foldername\\subfoldername\\";
-        
-        // Define file name format with current date
-        DateFormat dateFormat = new SimpleDateFormat("dd_MMM_yyyy");
-        String fileNameFormat = "BharaniDharan_" + dateFormat.format(new Date());
-        
-        // Define list of names
-        List<String> names = List.of("Aravind", "John", "BharaniDharan");
-        
-        // Check if file exists for each name
-        names.forEach(name -> {
-            String fileName = fileNameFormat + "_" + name + ".txt";
-            File file = new File(sharedPath + fileName);
-            
-            // Append name and status to report file
-            try (FileWriter writer = new FileWriter("report.txt", true)) {
-                if (file.exists()) {
-                    writer.write(name + " - Updated\n");
-                } else {
-                    writer.write(name + " - Not Updated\n");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        
-        // Read report file and send email
+        // Initialize file names and network path
+        List<String> names = new ArrayList<>();
+        names.add("Aravind");
+        names.add("John");
+        names.add("BharaniDharan");
+
+        String currentDate = new SimpleDateFormat("dd_MMM_yyyy").format(new Date());
+        String fileName = names.get(2) + "_" + currentDate;
+
+        String networkPath = "\\\\server\\share\\path\\";
+
+        // Check if file exists in network path
+        File file = new File(networkPath + fileName);
+        boolean fileExists = file.exists();
+
+        // Write to report file
         try {
-            String reportContent = Files.lines(Paths.get("report.txt")).collect(Collectors.joining(System.lineSeparator()));
-            String subject = "File Check Report";
-            String body = "Please find the attached report for file check status.";
-            String recipient = "recipient@example.com";
-            String attachmentPath = "report.txt";
-            
-            EmailSender.sendEmail(recipient, subject, body, attachmentPath, reportContent);
-            
+            FileWriter reportFileWriter = new FileWriter("report.txt", true);
+            if (fileExists) {
+                reportFileWriter.write(names.get(2) + " - updated\n");
+            } else {
+                reportFileWriter.write(names.get(2) + " - not updated\n");
+            }
+            reportFileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
+        // Read report file and send email
+        try {
+            File reportFile = new File("report.txt");
+            String emailBody = "Network File Check Report:\n\n";
+            emailBody += "Name\tStatus\n";
+            emailBody += "----------------------\n";
+
+            for (String name : names) {
+                boolean foundName = false;
+                String status = "not updated";
+                String line;
+                File fileReader = new File("report.txt");
+                java.util.Scanner sc = new java.util.Scanner(fileReader);
+                while (sc.hasNextLine()) {
+                    line = sc.nextLine();
+                    if (line.startsWith(name)) {
+                        foundName = true;
+                        if (line.endsWith("updated")) {
+                            status = "updated";
+                        }
+                        break;
+                    }
+                }
+                sc.close();
+                if (!foundName) {
+                    status = "not found";
+                }
+                emailBody += name + "\t" + status + "\n";
+            }
+
+            EmailSender emailSender = new EmailSender();
+            emailSender.setMailServer("smtp.gmail.com");
+            emailSender.setMailServerPort("465");
+            emailSender.setSenderAddress("sender@gmail.com");
+            emailSender.setSenderPassword("password");
+            emailSender.setReceiverAddress("receiver@gmail.com");
+            emailSender.setSubject("Network File Check Report");
+            emailSender.setBody(emailBody);
+            emailSender.setAttachment(reportFile);
+            emailSender.sendEmail();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
